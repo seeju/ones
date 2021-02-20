ddf76f40fa6fa24a90fc7e78642c5d3b183853ba
seeju <julianap.fer@gmail.com>
2021-02-20 17:51:10 -0300

    Teste de Link

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
 import { useState } from 'react';
+import Link from 'next/link';
 
 function Home() {
     return (
     <div>
         <h1>Home</h1>
         <Contador />
+
+        <Link href="/sobre">
+            <a>Acessar página sobre</a>
+        </Link>
     </div>
     )
 }
